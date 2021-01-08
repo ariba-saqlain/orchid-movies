71a0e8140acbee86f7a07244d0cd00da78a6dc21
Ariba <ariba.saqlain@gmail.com>
2021-01-08 17:39:08 -0500

    changed API urls from HTTP to HTTPS

--- src/data/movieList.js
+++ src/data/movieList.js
@@ -4,97 +4,97 @@ export const movieList = [
     title: "A Beautiful Mind",
     src:
       "https://m.media-amazon.com/images/M/MV5BMzcwYWFkYzktZjAzNC00OGY1LWI4YTgtNzc5MzVjMDVmNjY0XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
-    apiLink: "http://omdbapi.com/?i=tt0268978&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt0268978&apikey=1001f938",
   },
   {
     id: "1",
     title: "Batman Begins",
     src:
       "https://images-na.ssl-images-amazon.com/images/I/71uMbCnPVQL._AC_SL1259_.jpg",
-    apiLink: "http://omdbapi.com/?i=tt0372784&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt0372784&apikey=1001f938",
   },
   {
     id: "2",
     title: "Captain Phillips",
     src:
       "https://m.media-amazon.com/images/M/MV5BODhiZWRhMjctNDUyMS00NmUwLTgwYmItMjJhOWNkZWQ3ZTQxXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
-    apiLink: "http://omdbapi.com/?i=tt1535109&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt1535109&apikey=1001f938",
   },
   {
     id: "3",
     title: "Fight Club",
     src:
       "https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
-    apiLink: "http://omdbapi.com/?i=tt0137523&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt0137523&apikey=1001f938",
   },
   {
     id: "4",
     title: "Freedom Writers",
     src:
       "https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/83877/83449/freedom_writers_double_sided_international_movie_poster_buy_now_at_starstills_1706__87240__18304.1394513281.jpg?c=2?imbypass=on",
-    apiLink: "http://omdbapi.com/?i=tt0463998&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt0463998&apikey=1001f938",
   },
   {
     id: "5",
     title: "Good Will Hunting",
     src:
       "https://m.media-amazon.com/images/M/MV5BOTI0MzcxMTYtZDVkMy00NjY1LTgyMTYtZmUxN2M3NmQ2NWJhXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
-    apiLink: "http://omdbapi.com/?i=tt0119217&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt0119217&apikey=1001f938",
   },
   {
     id: "6",
     title: "Inside Man",
     src:
       "https://m.media-amazon.com/images/M/MV5BYjc4MjA2ZDgtOGY3YS00NDYzLTlmNTEtYWMxMzcwZjgzYWNjXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
-    apiLink: "http://omdbapi.com/?i=tt0454848&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt0454848&apikey=1001f938",
   },
   {
     id: "7",
     title: "Joker",
     src:
       "https://i.pinimg.com/originals/dd/58/62/dd586203382ee10102c591bff2a551b8.jpg",
-    apiLink: "http://omdbapi.com/?i=tt7286456&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt7286456&apikey=1001f938",
   },
   {
     id: "8",
     title: "Seven Pounds",
     src:
       "https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/chameleon/title-movie/230909_SEVEN%20POUNDS_1400x2100_English.jpg?itok=bu1H5nv3",
-    apiLink: "http://omdbapi.com/?i=tt0814314&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt0814314&apikey=1001f938",
   },
   {
     id: "9",
     title: "Shutter Island",
     src:
       "https://m.media-amazon.com/images/M/MV5BYzhiNDkyNzktNTZmYS00ZTBkLTk2MDAtM2U0YjU1MzgxZjgzXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
-    apiLink: "http://omdbapi.com/?i=tt1130884&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt1130884&apikey=1001f938",
   },
   {
     id: "10",
     title: "The Dark Knight",
     src:
       "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg",
-    apiLink: "http://omdbapi.com/?i=tt0468569&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt0468569&apikey=1001f938",
   },
   {
     id: "11",
     title: "The Dark Knight Rises",
     src:
       "https://i.pinimg.com/originals/65/23/c2/6523c21f0e7c334bedc126e55bbaec98.jpg",
-    apiLink: "http://omdbapi.com/?i=tt1345836&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt1345836&apikey=1001f938",
   },
   {
     id: "12",
     title: "The Pursuit of Happyness",
     src:
       "https://m.media-amazon.com/images/M/MV5BMTQ5NjQ0NDI3NF5BMl5BanBnXkFtZTcwNDI0MjEzMw@@._V1_.jpg",
-    apiLink: "http://omdbapi.com/?i=tt0454921&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt0454921&apikey=1001f938",
   },
   {
     id: "13",
     title: "The Truman Show",
     src:
       "https://i.etsystatic.com/23539062/r/il/359c4e/2465142831/il_570xN.2465142831_8uwl.jpg",
-    apiLink: "http://omdbapi.com/?i=tt0120382&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt0120382&apikey=1001f938",
   },
 ];
--- src/data/tvList.js
+++ src/data/tvList.js
@@ -4,96 +4,96 @@ export const tvList = [
     title: "Black Mirror",
     src:
       "https://i.pinimg.com/originals/fc/10/3a/fc103a99e0d4e0e4f18b71c8ff80e992.jpg",
-    apiLink: "http://omdbapi.com/?i=tt2085059&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt2085059&apikey=1001f938",
   },
   {
     id: "1",
     title: "Breaking Bad",
     src:
       "https://i.pinimg.com/736x/6a/c5/08/6ac508230512a6a2f3a2e5d80f84d6c2.jpg",
-    apiLink: "http://omdbapi.com/?i=tt0903747&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt0903747&apikey=1001f938",
   },
   {
     id: "2",
     title: "Dexter",
     src:
       "https://lh3.googleusercontent.com/hHumrLAfJtAmB0Vmty4_aHedT8e0Dd2U_iscmPlIFJtSG7gmM7lFfGqp3b0cILaxf9MYVsvZFQOpseDQCllYdIg=s1950",
-    apiLink: "http://omdbapi.com/?i=tt0773262&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt0773262&apikey=1001f938",
   },
   {
     id: "3",
     title: "Friends",
     src:
       "https://m.media-amazon.com/images/M/MV5BZDE1NjFkZmMtNGY1MC00OGFlLWFiY2MtODZlZWIyZmRkOTJiXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg",
-    apiLink: "http://omdbapi.com/?i=tt0108778&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt0108778&apikey=1001f938",
   },
   {
     id: "4",
     title: "Grey's Anatomy",
     src:
       "https://i.pinimg.com/originals/51/40/fc/5140fc824ba0179ba68c4b28d0aab644.jpg",
-    apiLink: "http://omdbapi.com/?i=tt0413573&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt0413573&apikey=1001f938",
   },
   {
     id: "5",
     title: "How I Met Your Mother",
     src:
       "https://images-na.ssl-images-amazon.com/images/I/61Gp6Ee2EZL._AC_SL1093_.jpg",
-    apiLink: "http://omdbapi.com/?i=tt0460649&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt0460649&apikey=1001f938",
   },
   {
     id: "6",
     title: "How To Get Away With Murder",
     src:
       "https://m.media-amazon.com/images/M/MV5BZDQ5ZDRhMWItNjA5Ni00MDhiLTgwN2EtZjFkYzgzNjBhZTIyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
-    apiLink: "http://omdbapi.com/?i=tt3205802&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt3205802&apikey=1001f938",
   },
   {
     id: "7",
     title: "Ozark",
     src:
       "https://www.kolpaper.com/wp-content/uploads/2020/05/Ozark-Wallpaper.jpg",
-    apiLink: "http://omdbapi.com/?i=tt5071412&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt5071412&apikey=1001f938",
   },
   {
     id: "8",
     title: "Prison Break",
     src:
       "https://i.pinimg.com/originals/6f/12/e9/6f12e9b6c3e1f5e2fca290d82955dec2.jpg",
-    apiLink: "http://omdbapi.com/?i=tt0455275&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt0455275&apikey=1001f938",
   },
   {
     id: "9",
     title: "Stranger Things",
     src:
       "https://pyxis.nymag.com/v1/imgs/9d6/af9/9729d1d29d1a6cae70558b80f09c81c0a5-11-stranger-things-s2.jpg",
-    apiLink: "http://omdbapi.com/?i=tt4574334&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt4574334&apikey=1001f938",
   },
   {
     id: "10",
     title: "The 100",
     src: "https://pbs.twimg.com/media/C23vjZ4WQAAbVnd.jpg",
-    apiLink: "http://omdbapi.com/?i=tt2661044&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt2661044&apikey=1001f938",
   },
   {
     id: "11",
     title: "The Good Place",
     src:
       "https://img.reelgood.com/content/show/0276f425-a764-445b-9ef4-b923042e6337/poster-780.jpg",
-    apiLink: "http://omdbapi.com/?i=tt4955642&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt4955642&apikey=1001f938",
   },
   {
     id: "12",
     title: "The Office",
     src:
       "https://i.pinimg.com/736x/0e/15/bb/0e15bbb4a0c6914b4c8008b0d395bb28.jpg",
-    apiLink: "http://omdbapi.com/?i=tt0386676&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt0386676&apikey=1001f938",
   },
   {
     id: "13",
     title: "This is Us",
     src:
       "https://i.pinimg.com/originals/c2/e8/d1/c2e8d10b38d37e837b0c1d9043cbd697.jpg",
-    apiLink: "http://omdbapi.com/?i=tt5555260&apikey=1001f938",
+    apiLink: "https://omdbapi.com/?i=tt5555260&apikey=1001f938",
   },
 ];
